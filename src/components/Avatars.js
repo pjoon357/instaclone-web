9261c39c6a86e3bc73e4137c606bade6e4836e23
pjoon357 <pjoon357@naver.com>
2021-08-05 17:43:25 +0900

    feat: see Feed onlu photo

--- src/components/Avatars.js
+++ src/components/Avatars.js
@@ -1,11 +1,9 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styled from "styled-components";
-import { faUser } from "@fortawesome/free-regular-svg-icons";
 
 const SAvatar = styled.div`
-  width: 25px;
-  height: 25px;
-  border-radius: 15px;
+  width: ${(props) => (props.lg ? "30px" : "25px")};
+  height: ${(props) => (props.lg ? "30px" : "25px")};
+  border-radius: 50%;
   background-color: #2c2c2c;
   overflow: hidden;
 `;
@@ -14,7 +12,7 @@ const Img = styled.img`
   max-width: 100%;
 `;
 
-function Avatar({ url = "" }) {
-    return <SAvatar>{url !== "" ? <Img src={url} /> : <FontAwesomeIcon icon={faUser} size="lg" />}</SAvatar>;
+function Avatar({ url = "", lg = false }) {
+    return <SAvatar lg={lg}>{url !== "" ? <Img src={url} /> : null}</SAvatar>;
 }
 export default Avatar;
