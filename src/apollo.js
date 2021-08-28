9640f42c58f01173aa34002ffb51da1aeada3cc1
pjoon357 <pjoon357@naver.com>
2021-08-28 17:25:22 +0900

    Production API URL

--- src/apollo.js
+++ src/apollo.js
@@ -27,7 +27,10 @@ export const disableDarkMode = () => {
 };
 
 const httpLink = createHttpLink({
-    uri: "http://localhost:4000/graphql",
+    uri:
+        process.env.NODE_ENV === "production"
+            ? "https://instaclone-be.herokuapp.com/graphql"
+            : "http://localhost:4000/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
